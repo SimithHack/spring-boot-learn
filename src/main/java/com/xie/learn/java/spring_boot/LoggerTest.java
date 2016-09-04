c5127f7634443450aa4608278ddf5d33901c51ac
SimithHack <xiefuqiang95@gmail.com>
2016-09-04 08:34:42 +0800

    add logger learn chapter

---
+++ src/main/java/com/xie/learn/java/spring_boot/LoggerTest.java
@@ -0,0 +1,13 @@
+package com.xie.learn.java.spring_boot;
+
+/**
+ * Created by xfq on 2016/9/4.
+ * 日志配置
+ * 默认日志的输出目的地是console，可以设置logging.file或者logging.path属性来指定日志输出文件路径
+ * logging.flile指定文件
+ * logging.path指定目录
+ * 10MB rotate
+ * 默认ERROR WARN INFO级别
+ */
+public class LoggerTest {
+}
